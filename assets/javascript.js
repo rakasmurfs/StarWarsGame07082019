7a6aa65613065a923e569643c53c1e0d8341cfa6
Miguel Wolmers <rakasmurfs@gmail.com>
2019-07-08 13:53:19 -0400

    Fixed new game button

--- assets/javascript.js
+++ assets/javascript.js
@@ -140,15 +140,135 @@ $("#result").html("");
 $("characters").html("");
 
 for(i=0; i< players.length; i++)
-{
-    var objectPos = players[i];
-    $("#characters").append('<div id =' + objectPos.idName +'></div>');
-  //  $("#players").attr('id', objectPos.idName);
-    $("#"+objectPos.idName).attr('class', objectPos.idName);
-    $("#"+objectPos.idName).append(objectPos.image);
-    $("#"+objectPos.idName).append("<span> <br>" + objectPos.name + "</span>" + "<br>"); //+ "<span class="+objectPos.idName + "Health>" + "HP: " + objectPos.health + "</span>");
-}
+    {
+        var objectPos = players[i];
+        $("#characters").append('<div id =' + objectPos.idName +'></div>');
+        $("#"+objectPos.idName).attr('class', objectPos.idName);
+        $("#"+objectPos.idName).append(objectPos.image);
+        $("#"+objectPos.idName).append("<span> <br>" + objectPos.name + "</span>" + "<br>"); //+ "<span class="
+    }
+    $("#darthMaul").on("click", function()
+    {
+        if(playerOneSelected === false)
+            {
+                $("#darthMaul").appendTo("#playerOne");
+                darthMinion.isPlayerOne = true;
+                playerOneSelected = true;
+                $("#jedi").appendTo("#enemies");
+                $("#yoda").appendTo("#enemies");
+                $("#trooper").appendTo("#enemies"); 
+                playerOneAttack = darthMinion.attackPower;
+                playerOneHealth = darthMinion.health;
+                playerOneBaseAttack = darthMinion.baseAttackPower;
+                $("#darthMaul").append("<span class = 'playerOneHP'>" + "HP: " + darthMinion.health + "</span>");
             }
+    }); 
+$("#jedi").on("click", function()
+    {
+        if(playerOneSelected === false)
+            {
+                $("#jedi").appendTo("#playerOne");
+                jediMinion.isPlayerOne = true;
+                playerOneSelected = true;
+                $("#darthMaul").appendTo("#enemies");
+                $("#yoda").appendTo("#enemies");
+                $("#trooper").appendTo("#enemies"); 
+                playerOneAttack = jediMinion.attackPower;
+                playerOneHealth = jediMinion.health;
+                playerOneBaseAttack = jediMinion.baseAttackPower;
+                $("#jedi").append("<span class = 'playerOneHP'>" + "HP: " + jediMinion.health + "</span>");
+            }
+    }); 
+$("#yoda").on("click", function()
+    {
+        if(playerOneSelected === false)
+            {
+                $("#yoda").appendTo("#playerOne");
+                yodaMinion.isPlayerOne = true;
+                playerOneSelected = true;
+                $("#darthMaul").appendTo("#enemies");
+                $("#jedi").appendTo("#enemies");
+                $("#trooper").appendTo("#enemies"); 
+                playerOneAttack = yodaMinion.attackPower;
+                playerOneBaseAttack = yodaMinion.baseAttackPower;
+                playerOneHealth = yodaMinion.health;
+                $("#yoda").append("<span class = 'playerOneHP'>" + "HP: " + yodaMinion.health + "</span>");
+            }
+    }); 
+$("#trooper").on("click", function()
+    {
+        if(playerOneSelected === false)
+            {
+                $("#trooper").appendTo("#playerOne");
+                trooperMinion.isPlayerOne = true;
+                playerOneSelected = true;
+                $("#darthMaul").appendTo("#enemies");
+                $("#jedi").appendTo("#enemies");
+                $("#yoda").appendTo("#enemies"); 
+                playerOneAttack = trooperMinion.attackPower;
+                playerOneHealth = trooperMinion.health;
+                playerOneBaseAttack = trooperMinion.baseAttackPower;
+                $("#trooper").append("<span class = 'playerOneHP'>" + "HP: " + trooperMinion.health + "</span>");
+                playerOneTrooper = true;
+            }
+    }); 
+    $("#darthMaul").on("click", function()
+    {
+        if(playerOneSelected === true && darthMinion.isPlayerOne ===false && playerTwoSelected === false)
+            {
+                $("#darthMaul").appendTo("#playerTwo");
+                darthMinion.isPlayerTwo = true;
+                playerTwoSelected = true;
+                playerTwoHealth = darthMinion.health;
+                playerTwoCounter = darthMinion.counterAttackPower;
+                $("#darthMaul").append("<span class = 'playerTwoHP'>" + "HP: " + darthMinion.health + "</span>");
+                playerTwoName = darthMinion.name;
+            }
+    }); 
+$("#trooper").on("click", function()
+    {
+        if(playerOneSelected === true && trooperMinion.isPlayerOne ===false && playerTwoSelected === false)
+            {
+                $("#trooper").appendTo("#playerTwo");
+                trooperMinion.isPlayerTwo = true;
+                playerTwoSelected = true;
+                playerTwoHealth = trooperMinion.health;
+                playerTwoCounter = trooperMinion.counterAttackPower;
+                $("#trooper").append("<span class = 'playerTwoHP'>" + "HP: " + trooperMinion.health + "</span>");
+                playerTwoName = trooperMinion.name;
+                playerTwoTrooper = true;
+            }
+    }); 
+$("#yoda").on("click", function()
+    {
+        if(playerOneSelected === true && yodaMinion.isPlayerOne ===false && playerTwoSelected === false)
+            {
+                $("#yoda").appendTo("#playerTwo");
+                yodaMinion.isPlayerTwo = true;
+                playerTwoSelected = true;
+                playerTwoHealth = yodaMinion.health;
+                playerTwoCounter = yodaMinion.counterAttackPower;
+                $("#yoda").append("<span class = 'playerTwoHP'>" + "HP: " + yodaMinion.health + "</span>");
+                playerTwoName = yodaMinion.name;
+            }
+    }); 
+$("#jedi").on("click", function()
+    {
+        if(playerOneSelected === true && jediMinion.isPlayerOne ===false && playerTwoSelected === false)
+            {
+                $("#jedi").appendTo("#playerTwo");
+                jediMinion.isPlayerTwo = true;
+                playerTwoSelected = true;
+                playerTwoHealth = jediMinion.health;
+                playerTwoCounter = jediMinion.counterAttackPower;
+                $("#jedi").append("<span class = 'playerTwoHP'>" + "HP: " + jediMinion.health + "</span>");
+                playerTwoName = jediMinion.name;
+            }
+    }); 
+            }
+
+
+
             //below generates each div/image of my objects
         for(i=0; i< players.length; i++)
             {
@@ -293,6 +413,7 @@ for(i=0; i< players.length; i++)
 
                         if(playerOneHealth > 0)
                             {
+                                // below is the trooper only attack hit chance
                                 if(playerOneTrooper===true)
                                     {
                                         random =Math.floor(Math.random() *101);
@@ -305,6 +426,7 @@ for(i=0; i< players.length; i++)
                                                 playerOneAttack = 0;
                                             }
                                     }
+                                    //end trooper only attack hit chance
                                 playerTwoHealth-=playerOneAttack;
                                 if(playerTwoHealth>0)
                                     {
